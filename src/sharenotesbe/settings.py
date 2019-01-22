e77c17bebcf1e1c4736e5dea34dfacf1a122906f
anantanant2015 <anantanant2015@gmail.com>
2019-01-22 22:29:04 +0530

    Adds permission to allow any request.

--- src/sharenotesbe/settings.py
+++ src/sharenotesbe/settings.py
@@ -121,4 +121,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+REST_FRAMEWORK = {
+	'DEFAULT_PERMISSION_CLASSES': [
+		# 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
+	]
+}
+
 CORS_ORIGIN_ALLOW_ALL = True
