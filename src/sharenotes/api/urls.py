dbf5f1c200ba6b054833c199ce01a3b8f689ea93
anantanant2015 <anantanant2015@gmail.com>
2019-01-22 18:14:05 +0530

    Adds viewset in api views, and updates api urls.

--- src/sharenotes/api/urls.py
+++ src/sharenotes/api/urls.py
@@ -1,36 +1,8 @@
-from django.urls import path
-from .views import (
-	NoteListView,
-	NoteDetailView,
-	NoteCreateView,
-	NoteUpdateView,
-	NoteDeleteView,
-	UserListView,
-	UserDetailView,
-	UserCreateView,
-	UserUpdateView,
-	UserDeleteView,
-	UserNoteListView,
-	UserNoteDetailView,
-	UserNoteCreateView,
-	UserNoteUpdateView,
-	UserNoteDeleteView
-)
+from sharenotes.api.views import NoteViewSet, UserViewSet, UserNoteViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-	path('note/', NoteListView.as_view()),
-	path('note-create/', NoteCreateView.as_view()),
-	path('note/<pk>', NoteDetailView.as_view()),
-	path('note/<pk>/update/', NoteUpdateView.as_view()),
-	path('note/<pk>/delete/', NoteDeleteView.as_view()),
-	path('user/', UserListView.as_view()),
-	path('user-create/', UserCreateView.as_view()),
-	path('user/<pk>', UserDetailView.as_view()),
-	path('user/<pk>/update/', UserUpdateView.as_view()),
-	path('user/<pk>/delete/', UserDeleteView.as_view()),
-	path('usernote/', UserNoteListView.as_view()),
-	path('usernote-create/', UserNoteCreateView.as_view()),
-	path('usernote/<pk>', UserNoteDetailView.as_view()),
-	path('usernote/<pk>/update/', UserNoteUpdateView.as_view()),
-	path('usernote/<pk>/delete/', UserNoteDeleteView.as_view())
-]
+router = DefaultRouter()
+router.register(r'note', NoteViewSet, base_name='note')
+router.register(r'user', UserViewSet, base_name='user')
+router.register(r'usernote', UserNoteViewSet, base_name='usernote')
+urlpatterns = router.urls
--- src/sharenotes/api/views.py
+++ src/sharenotes/api/views.py
@@ -1,74 +1,19 @@
-from rest_framework.generics import (
-    ListAPIView, 
-    RetrieveAPIView,
-    CreateAPIView,
-	UpdateAPIView,
-	DestroyAPIView
-)
+from rest_framework import viewsets
 from sharenotes.models import Note, User, UserNote
 from .serializers import NoteSerializer, UserSerializer, UserNoteSerializer
 
 
-class NoteListView(ListAPIView):
-    queryset = Note.objects.all()
+class NoteViewSet(viewsets.ModelViewSet):
     serializer_class = NoteSerializer
-
-class NoteDetailView(RetrieveAPIView):
     queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-
-class NoteCreateView(CreateAPIView):
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-
-class NoteUpdateView(UpdateAPIView):
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-
-class NoteDeleteView(DestroyAPIView):
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
 
 
-class UserListView(ListAPIView):
-    queryset = User.objects.all()
+class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-
-
-class UserDetailView(RetrieveAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserCreateView(CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserUpdateView(UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserDeleteView(DestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 
-class UserNoteListView(ListAPIView):
-    queryset = UserNote.objects.all()
+class UserNoteViewSet(viewsets.ModelViewSet):
     serializer_class = UserNoteSerializer
-
-
-class UserNoteDetailView(RetrieveAPIView):
     queryset = UserNote.objects.all()
-    serializer_class = UserNoteSerializer
 
-class UserNoteCreateView(CreateAPIView):
-    queryset = UserNote.objects.all()
-    serializer_class = UserNoteSerializer
-
-class UserNoteUpdateView(UpdateAPIView):
-    queryset = UserNote.objects.all()
-    serializer_class = UserNoteSerializer
-
-class UserNoteDeleteView(DestroyAPIView):
-    queryset = UserNote.objects.all()
-    serializer_class = UserNoteSerializer
